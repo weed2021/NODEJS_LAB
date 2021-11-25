d222f3bd3c42995cd50fdfa83df5556fa74811b4
bokute2010 <lequanvt299@gmail.com>
2021-11-25 10:26:09 +0700

    lab1_4

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const http = require('http');
 const server = http.createServer((req,res) =>{
+    const url = req.url;
+    if (url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name = "message"><button type = "submit">Send</button></form></body>')
+        res.write('</html>');
+        return res.end();
+    }
+
     //console.log(req)
     //process.exit();
     res.setHeader('Content-Type','text/html');
