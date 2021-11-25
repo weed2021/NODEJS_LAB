bb07ee9f4fbc8b85b75bf6cdfa6556a1d18b2ebb
bokute2010 <lequanvt299@gmail.com>
2021-11-25 10:52:47 +0700

    lab1_5

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req,res) =>{
     const url = req.url;
+    const method = req.method;
     if (url==='/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
@@ -8,7 +10,12 @@ const server = http.createServer((req,res) =>{
         res.write('</html>');
         return res.end();
     }
-
+    if(url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.text','DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
     //console.log(req)
     //process.exit();
     res.setHeader('Content-Type','text/html');
