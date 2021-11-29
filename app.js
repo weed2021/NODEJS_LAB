ce7a630815519c6142091ba3ebb425299a1785f1
bokute2010 <lequanvt299@gmail.com>
2021-11-29 18:11:07 +0700

    lab2_6

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type = "text" name = "title"> <button type = "submit">Add product</button></form>');
 });
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
