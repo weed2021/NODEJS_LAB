44cda36a68ff7d56bac5371b7f4f79e28408c05f
bokute2010 <lequanvt299@gmail.com>
2021-11-29 14:18:46 +0700

    lab2_3

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another middleware');
+    res.send('Hello from Express.js')
 });
 
 const server = http.createServer(app)
