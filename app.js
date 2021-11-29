4bced527f29cc301fb65badb07c40f462a26633e
bokute2010 <lequanvt299@gmail.com>
2021-11-29 14:57:07 +0700

    lab2_4

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); //Allows the request to continue to the next middleware in line
+app.use('/',(req, res, next) => {
+    console.log('This always runs!');
+    next();
 });
-
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
+    console.log('In another middleware');
+    res.send('The "Add Product" Page');
+});
+app.use('/',(req, res, next) => {
     console.log('In another middleware');
     res.send('Hello from Express.js')
+    next();
 });
 
-const server = http.createServer(app)
 
-server.listen(3000)
+
+
+app.listen(3000)
+
