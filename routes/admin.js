3f11bd5e9784389c4ab7fa3bccd44f72463357c0
bokute2010 <lequanvt299@gmail.com>
2021-12-02 11:11:22 +0700

    lab2_9

--- routes/admin.js
+++ routes/admin.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type = "text" name = "title"> <button type = "submit">Add product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type = "text" name = "title"> <button type = "submit">Add product</button></form>');
 });
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
