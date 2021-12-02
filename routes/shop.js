a27100c715b9b1ba3a7cb4340b11479bfb62a323
bokute2010 <lequanvt299@gmail.com>
2021-12-02 15:39:26 +0700

    lab3_2

--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,9 @@ const rootDir = require('../util/path')
 const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products)
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.render('shop')
+    // console.log(adminData.products)
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
