48fd4309d58624f105da2bedc678306aa8b8b3ad
bokute2010 <lequanvt299@gmail.com>
2021-12-02 11:53:09 +0700

    lab2_11

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
-app.use((req,res,next) =>{
-    res.status(404).send('<h1>Page cannot found</h1>')
-})
+// app.use((req,res,next) =>{
+//     res.status(404).send('<h1>Page cannot found</h1>')
+// })
 app.listen(3000)
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-    res.send('Hello from Express.js')
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
