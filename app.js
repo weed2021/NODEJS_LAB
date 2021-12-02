ab1a0928b42efce635e307979538f0b722198096
bokute2010 <lequanvt299@gmail.com>
2021-12-02 12:00:13 +0700

    lab2_12

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
-// app.use((req,res,next) =>{
-//     res.status(404).send('<h1>Page cannot found</h1>')
-// })
+app.use((req,res,next) =>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
 app.listen(3000)
 
