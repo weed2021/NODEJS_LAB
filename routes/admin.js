87c346fa13009bb200a44a8734f436ab66e3488f
bokute2010 <lequanvt299@gmail.com>
2021-12-02 14:47:41 +0700

    lab3_1

--- routes/admin.js
+++ routes/admin.js
@@ -4,15 +4,19 @@ const express = require('express');
 
 const router = express.Router();
 
-const rootDir = require('../util/path')
+const rootDir = require('../util/path');
+
+const products = [];
 
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
 
-module.exports = router
+// module.exports = router
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -6,7 +6,10 @@ const router = express.Router();
 
 const rootDir = require('../util/path')
 
+const adminData = require('./admin')
+
 router.get('/', (req, res, next) => {
+    console.log(adminData.products)
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
