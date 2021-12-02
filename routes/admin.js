8d4142d4fecbe0e51ed68f73fc4229b496eb19e5
bokute2010 <lequanvt299@gmail.com>
2021-12-02 12:21:22 +0700

    lab2_13

--- routes/admin.js
+++ routes/admin.js
@@ -4,8 +4,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const rootDir = require('../util/path')
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
