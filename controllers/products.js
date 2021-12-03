851a9538306e8652968be3895c6e7327f39765cf
bokute2010 <lequanvt299@gmail.com>
2021-12-03 23:11:37 +0700

    lab4_3

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {
@@ -13,11 +13,14 @@ exports.getAddProduct = (req, res, next) => {
 
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
+    //products.push({ title: req.body.title });
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
