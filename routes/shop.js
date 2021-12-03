5d53f24d4d7ef977b2049c211a6e4e65bff10682
bokute2010 <lequanvt299@gmail.com>
2021-12-03 13:24:02 +0700

    lab3_9

--- routes/shop.js
+++ routes/shop.js
@@ -12,14 +12,11 @@ router.get('/', (req, res, next) => {
     const products = adminData.products;
     res.render('shop', {
         prods: products,
-        docTitle: 'Shop',
+        pageTitle: 'Shop',
         path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+
     });
-    // console.log(adminData.products)
-    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
 });
 
 module.exports = router;
