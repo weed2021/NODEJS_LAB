84bc58564efe088cd84a6e16273ee71b7f39fbfd
bokute2010 <lequanvt299@gmail.com>
2021-12-04 10:46:24 +0700

    lab4_6

--- models/product.js
+++ models/product.js
@@ -1,45 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+const getProductsFromFile = cb => {
+
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+
+    });
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t
     };
 
     save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent)
-            }
-            //console.log(err);
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
-            })
-
+            });
         });
     };
 
     static fetchAll(cb) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-
-        fs.readFile(p,(err,fileContent) => {
-            if(err){
-                cb([]);
-            };
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     };
 
     //Static can call directly from class Product
