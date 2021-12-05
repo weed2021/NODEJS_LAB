929318b5dd5c2a4520cf19b56e3d4836263101fe
bokute2010 <lequanvt299@gmail.com>
2021-12-05 14:16:47 +0700

    lab4_16

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     //products.push({ title: req.body.title });
     res.redirect('/');
@@ -23,12 +23,12 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
-    if(!editMode){
+    if (!editMode) {
         return res.redirect('/');
     }
     const prodId = req.params.productId; //Request Url params
-    Product.findById(prodId, product =>{
-        if(!product){
+    Product.findById(prodId, product => {
+        if (!product) {
             return res.redirect('/');
         }
         res.render('admin/edit-product', {
@@ -39,12 +39,22 @@ exports.getEditProduct = (req, res, next) => {
         });
     });
 
-
-    
 };
 
-exports.postEditProduct =(req,res,next) =>{
-    
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(prodId,
+        updatedTitle,
+        updatedImageUrl,
+        updatedDesc,
+        updatedPrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
+
 }
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -7,10 +7,12 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 
 router.get('/add-product', adminController.getAddProduct);
+
 router.get('/products',adminController.getProducts);
 
 router.get('/edit-product/:productId',adminController.getEditProduct);
-router.post('edit-product');
+
+router.post('/edit-product',adminController.postEditProduct);
 
 router.post('/add-product', adminController.postAddProduct);
 
