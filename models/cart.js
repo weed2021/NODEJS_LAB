b9d8462fff23021c135ce09bac50cbb21e0607e1
bokute2010 <lequanvt299@gmail.com>
2021-12-06 15:13:50 +0700

    lab4_20

--- models/cart.js
+++ models/cart.js
@@ -47,7 +47,7 @@ module.exports = class Cart {
                 return;
             }
             const updatedCart = { ...JSON.parse(fileContent)};
-            const product = updatedCart.products.find(prod => prod.id = id);
+            const product = updatedCart.products.find(prod => prod.id === id);
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
