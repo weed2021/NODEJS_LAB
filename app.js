f884c24c761fdcdcc235f9fcc44bad211eaa8ea8
bokute2010 <lequanvt299@gmail.com>
2021-12-07 16:53:30 +0700

    lab5_18

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -15,20 +17,41 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next)=>{
+    User.findByPk(1)
+        .then(user =>{
+            req.user = user;
+        })
+        .catch(err=>{console.log(err)})
+})
+
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use(errorController.get404)
 
+Product.belongsTo(User,{constraints:true,onDelete: 'CASCADE'});
+User.hasMany(Product); //Can ignore, Just for clear
 
 sequelize
+    //.sync({force:true}) //Force drop all table to recreate new which had relation
     .sync()
     .then(result =>{
         //console.log(result);
+        return User.findByPk(1);
+    })
+    .then(user =>{
+        if(!user){
+            User.create({name:'Zack',email:'test@test.com'});
+        }
+        return user;
+    })
+    .then(user =>{
+        //console.log(user);
         app.listen(3000);
     })
     .catch(err =>{
         console.log(err);
     });
-
+    
 
