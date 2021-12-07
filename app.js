6a41671f7d28aa651a815e6a4d90a35aa50c7db2
bokute2010 <lequanvt299@gmail.com>
2021-12-07 11:02:48 +0700

    lab5_10

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -19,5 +19,16 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use(errorController.get404)
-app.listen(3000)
+
+
+sequelize
+    .sync()
+    .then(result =>{
+        console.log(result);
+        app.listen(3000);
+    })
+    .catch(err =>{
+        console.log(err);
+    });
+
 
