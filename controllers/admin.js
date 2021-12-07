97e31111393368f66049683469efcd0795180789
bokute2010 <lequanvt299@gmail.com>
2021-12-07 17:24:31 +0700

    lab5_20

--- controllers/admin.js
+++ controllers/admin.js
@@ -38,8 +38,13 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId; //Request Url params
-    Product.findByPk(prodId)
-        .then(product => {
+
+    req.user.getProducts({ where: { id: prodId } })
+        .then(products => {
+            const product = products[0]
+            if (!product) {
+                return res.redirect('/');
+            }
             res.render('admin/edit-product', {
                 pageTitle: 'Edit product page',
                 path: '/admin/edit-product',
@@ -79,7 +84,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll()
+    req.user.getProducts()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
