37c3cca5ed38389c27267db6b6ea40d492f7497f
bokute2010 <lequanvt299@gmail.com>
2021-12-08 15:42:57 +0700

    lab6_1

--- app.js
+++ app.js
@@ -2,75 +2,34 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const CartItem = require('./models/cart-item');
-const Cart = require('./models/cart');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoClient = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => { console.log(err) })
+    // User.findByPk(1)
+    //     .then(user => {
+    //         req.user = user;
+    //         next();
+    //     })
+    //     .catch(err => { console.log(err) })
 })
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+// app.use('/admin', adminRoutes)
+// app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product); //Can ignore, Just for clear
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, {through:OrderItem});
-Product.belongsToMany(Order, {through:OrderItem});
-
-
-
-sequelize
-    //.sync({force:true}) //Force drop all table to recreate new which had relation
-    .sync()
-    .then(result => {
-        //console.log(result);
-        return User.findByPk(1);
-    })
-    .then(user => {
-        if (!user) {
-            User.create({ name: 'Zack', email: 'test@test.com' });
-        }
-        return user;
-    })
-    .then(user => {
-        //console.log(user);
-        //return user.createCart();
-        
-    })
-    .then(cart=>{
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-
+mongoClient((client)=>{
+    console.log(client);
+    app.listen(3000);
+})
--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root','funix',{
-    dialect: 'mysql',
-    host: 'localhost'
-})
+const mongoConnect = callback =>{
+    MongoClient.connect('mongodb+srv://funix:funix@cluster0.xox99.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(client =>{
+        
+        console.log('Connected');
+        callback(client)
+    })
+    .catch(err => console.log(err))
+}
 
-module.exports = sequelize
+module.exports = mongoConnect;
