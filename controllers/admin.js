3a4d626f7967a3a5d05db9119c932921376d3ac7
bokute2010 <lequanvt299@gmail.com>
2021-12-08 16:51:40 +0700

    lab6_5

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,14 +15,8 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-
-    req.user
-        .createProduct({
-            title: title,
-            price: price,
-            imageUrl: imageUrl,
-            description: description
-        })
+    const product = new Product(title,price, description,imageUrl);
+    product.save()
         .then(result => {
             console.log('Created product');
             res.redirect('/admin/products')
