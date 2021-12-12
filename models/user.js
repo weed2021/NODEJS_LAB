73fe73591b7b7cd34cf6d77fd8677e0641a4db1e
bokute2010 <lequanvt299@gmail.com>
2021-12-12 11:44:45 +0700

    lab6_24

--- models/user.js
+++ models/user.js
@@ -98,6 +98,14 @@ class User {
             })
     }
 
+    getOrders(){
+        const db = getDb();
+        return db
+            .collection('orders')
+            .find({'user._id': new ObjectId(this._id)}) //Compare
+            .toArray();
+    }
+
     static findById(userId) {
         const db = getDb();
         return db.collection('users').findOne({ _id: new ObjectId(userId) })
