f175b16732ca2d558e50e5b42dff9742be7eb0b6
bokute2010 <lequanvt299@gmail.com>
2021-12-12 17:17:47 +0700

    lab7_13

--- controllers/shop.js
+++ controllers/shop.js
@@ -78,7 +78,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     req.user
-        .deleteItemFromCart(prodId)
+        .removeFromCart(prodId)
         .then(() => {
             res.redirect('/cart')
         })
