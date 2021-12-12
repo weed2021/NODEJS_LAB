bf621c9b4be9a5ef07f311df05d480ceb276ddb9
bokute2010 <lequanvt299@gmail.com>
2021-12-12 18:45:26 +0700

    lab7_17

--- controllers/shop.js
+++ controllers/shop.js
@@ -121,19 +121,18 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-    req.user.getOrders({ include: ['products'] })
-        .then(orders => {
-            console.log(orders);
-            res.render('shop/orders', {
-                path: '/orders',
-                pageTitle: 'Your Orders',
-                orders: orders
-            });
-        })
-        .catch(err => {
-            console.log(err)
+    Order.find({'user.userId': req.user._id})
+    .then(orders => {
+        console.log(orders);
+        res.render('shop/orders', {
+            path: '/orders',
+            pageTitle: 'Your Orders',
+            orders: orders
         });
-
+    })
+    .catch(err => {
+        console.log(err)
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
