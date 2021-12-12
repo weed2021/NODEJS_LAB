bfe9dcbb4137876c3acc91b6ebe293e7b7a71401
bokute2010 <lequanvt299@gmail.com>
2021-12-12 18:18:52 +0700

    lab7_14

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-// const Order = require('../models/order');
+const Order = require('../models/order');
 
 
 exports.getProducts = (req, res, next) => {
@@ -89,13 +89,32 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
     req.user
-        .addOrder()
+        .populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items.map(i=>{
+                return {quantity:i.quantity,product:i.productId};
+            });
+            const order = new Order({
+                user: {
+                    name: req.user.name,
+                    userId: req.user
+                },
+                products: products
+            });
+            return order.save();
+        })
         .then(() => {
             res.redirect('/orders');
         })
         .catch(err => {
             console.log(err)
         });
+    
+
+
+
+
+
 }
 
 exports.getOrders = (req, res, next) => {
