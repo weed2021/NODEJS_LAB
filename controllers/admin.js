ca9ca0196382009d6157b942fef6f09bd9a13bee
bokute2010 <lequanvt299@gmail.com>
2021-12-12 15:30:35 +0700

    lab7_6

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,15 +62,14 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
 
-    const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        prodId
-    );
-
-    product.save()
+    Product.findById(prodId)
+        .then(product =>{
+            product.title = updatedTitle,
+            product.price = updatedPrice,
+            product.description = updatedDesc,
+            product.imageUrl = updatedImageUrl
+            return product.save()
+        })
         .then(result => {
             console.log('Updated Product');
             res.redirect('/admin/products');
@@ -81,7 +80,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
