c70d261ec98b414c2b6db74dfd5f8cee8e847f71
bokute2010 <lequanvt299@gmail.com>
2021-12-12 10:49:19 +0700

    lab6_22

--- controllers/shop.js
+++ controllers/shop.js
@@ -78,7 +78,7 @@ exports.postCart = (req, res, next) => {
         .then(product => {
             return req.user.addToCart(product);
         })
-        .then(()=>{
+        .then(() => {
             res.redirect('/cart');
         })
         .catch(err => {
@@ -100,30 +100,9 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
     let fetchedCart;
-    req.user.getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts();
-        })
-        .then(products => {
-            return req.user
-                .createOrder()
-                .then(order => {
-                    return order.addProducts(
-                        products.map(product => {
-                            product.orderItem = { quantity: product.cartItem.quantity };
-                            return product;
-                        })
-                    );
-                })
-                .catch(err => {
-                    console.log(err)
-                });
-        })
-        .then(result => {
-            return fetchedCart.setProducts(null);
-        })
-        .then(result => {
+    req.user
+        .addOrder()
+        .then(() => {
             res.redirect('/orders');
         })
         .catch(err => {
