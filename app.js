10488c3d63c3c7b790690f83e85907fda8c320e0
bokute2010 <lequanvt299@gmail.com>
2021-12-21 18:09:54 +0700

    lab8_5

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
+const session = require('express-session')
 
 const User = require('./models/user');
 const app = express();
@@ -16,6 +17,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret',resave: false, saveUninitialized: false}))
 
 app.use((req, res, next) => {
     User.findById('61b5b8e8fccae81215041e2d')
