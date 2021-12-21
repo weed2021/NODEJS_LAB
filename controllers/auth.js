c717ab979c760eb6340ffa8c154c348492d76105
bokute2010 <lequanvt299@gmail.com>
2021-12-21 23:22:32 +0700

    lab8_8

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
+
+const User = require('../models/user');
+
 exports.getLogin = (req,res,next) =>{
     // const isLoggedIn = req.get('Cookie').trim().split('=')[1];
 
-    console.log(req.session.isLoggedIn);
+    // console.log(req.session.isLoggedIn);
     res.render('auth/login',{
         path: '/login',
         pageTitle: 'Login',
@@ -10,6 +13,17 @@ exports.getLogin = (req,res,next) =>{
 }
 
 exports.postLogin = (req,res,next) =>{
-    req.session.isLoggedIn = true;
-    res.redirect('/')
+    User.findById('61b5b8e8fccae81215041e2d').then(user =>{
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        res.redirect('/');
+    })
+    .catch(err =>{ console.log(err)})
+}
+
+exports.postLogout = (req,res,next) =>{
+    req.session.destroy((err)=>{
+        console.log(err)
+        res.redirect('/');
+    })
 }
--- controllers/shop.js
+++ controllers/shop.js
@@ -9,7 +9,7 @@ exports.getProducts = (req, res, next) => {
                 prods: products,
                 pageTitle: 'All Products',
                 path: '/products',
-                isAuthenticated: isLoggedIn
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => { console.log(err) });
@@ -24,7 +24,7 @@ exports.getProduct = (req, res, next) => {
                 product: product,
                 pageTitle: product.title,
                 path: '/products',
-                isAuthenticated: isLoggedIn
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => {
@@ -40,7 +40,7 @@ exports.getIndex = (req, res, next) => {
                 prods: products,
                 pageTitle: 'Shop',
                 path: '/',
-                isAuthenticated: isLoggedIn
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => {
@@ -59,7 +59,7 @@ exports.getCart = (req, res, next) => {
                 path: '/cart',
                 pageTitle: 'Your Cart',
                 products: products,
-                isAuthenticated: isLoggedIn
+                isAuthenticated: req.isLoggedIn
             });
         })
         .catch(err => { console.log(err) });
@@ -131,7 +131,7 @@ exports.getOrders = (req, res, next) => {
             path: '/orders',
             pageTitle: 'Your Orders',
             orders: orders,
-            isAuthenticated: isLoggedIn
+            isAuthenticated: req.isLoggedIn
         });
     })
     .catch(err => {
@@ -143,6 +143,6 @@ exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
         pageTitle: 'Checkout',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: req.isLoggedIn
     });
 };
