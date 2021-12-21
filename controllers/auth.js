dc4d2f83b4660c88c592c7a1e7000a50bac4b08e
bokute2010 <lequanvt299@gmail.com>
2021-12-21 23:46:12 +0700

    lab8_9

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,6 @@
 const User = require('../models/user');
 
 exports.getLogin = (req,res,next) =>{
-    // const isLoggedIn = req.get('Cookie').trim().split('=')[1];
-
-    // console.log(req.session.isLoggedIn);
     res.render('auth/login',{
         path: '/login',
         pageTitle: 'Login',
--- controllers/shop.js
+++ controllers/shop.js
@@ -9,7 +9,7 @@ exports.getProducts = (req, res, next) => {
                 prods: products,
                 pageTitle: 'All Products',
                 path: '/products',
-                isAuthenticated: req.isLoggedIn
+                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => { console.log(err) });
@@ -24,7 +24,7 @@ exports.getProduct = (req, res, next) => {
                 product: product,
                 pageTitle: product.title,
                 path: '/products',
-                isAuthenticated: req.isLoggedIn
+                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => {
@@ -40,7 +40,7 @@ exports.getIndex = (req, res, next) => {
                 prods: products,
                 pageTitle: 'Shop',
                 path: '/',
-                isAuthenticated: req.isLoggedIn
+                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => {
@@ -59,7 +59,7 @@ exports.getCart = (req, res, next) => {
                 path: '/cart',
                 pageTitle: 'Your Cart',
                 products: products,
-                isAuthenticated: req.isLoggedIn
+                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => { console.log(err) });
@@ -131,7 +131,7 @@ exports.getOrders = (req, res, next) => {
             path: '/orders',
             pageTitle: 'Your Orders',
             orders: orders,
-            isAuthenticated: req.isLoggedIn
+            isAuthenticated: req.session.isLoggedIn
         });
     })
     .catch(err => {
@@ -143,6 +143,6 @@ exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
         pageTitle: 'Checkout',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
     });
 };
