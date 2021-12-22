34596d06fde2e84093a42fca705a7efd0087e190
bokute2010 <lequanvt299@gmail.com>
2021-12-22 13:56:46 +0700

    lab8_20

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf =  require('csurf');
 
 const User = require('./models/user');
 
@@ -16,6 +17,7 @@ const store = new MongoDBStore({
     uri: MONGODB_URI,
     collection: 'sessions'
 })
+const csrfProtecttion = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views')
@@ -27,6 +29,9 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }))
+
+
+app.use(csrfProtecttion);
 
 app.use((req, res, next) => {
     if(!req.session.user){
--- controllers/shop.js
+++ controllers/shop.js
@@ -40,7 +40,8 @@ exports.getIndex = (req, res, next) => {
                 prods: products,
                 pageTitle: 'Shop',
                 path: '/',
-                isAuthenticated: req.session.isLoggedIn
+                isAuthenticated: req.session.isLoggedIn,
+                csrfToken: req.csrfToken()   //This method provide by csurf package
             });
         })
         .catch(err => {
