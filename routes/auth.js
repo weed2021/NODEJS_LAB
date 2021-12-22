e1c55362233ecde410b7a0751ad0c65b5e9745eb
bokute2010 <lequanvt299@gmail.com>
2021-12-22 15:56:14 +0700

    lab9_3

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,26 @@
 const express = require('express');
 
-const {check} = require('express-validator/check');
+const { check } = require('express-validator/check');
 
 const router = express.Router();
 
 const authController = require('../controllers/auth');
 
 
-router.get('/login',authController.getLogin);
-router.post('/login',authController.postLogin);
-router.post('/logout',authController.postLogout);
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignup);
-router.post('/signup',check('email').isEmail().withMessage('Please enter valid email!'), authController.postSignup);
+router.post('/signup',
+    check('email')
+        .isEmail()
+        .withMessage('Please enter valid email!')
+        .custom((value,{req})=>{
+            if(value ==='test@test.com'){
+                throw new Error('This email address forbidden.');
+            }
+            return true;
+        }),
+    authController.postSignup);
 
 module.exports = router;
