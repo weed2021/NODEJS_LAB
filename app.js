277c16a449d482f57ce43ab51aa572cf51bf9f11
bokute2010 <lequanvt299@gmail.com>
2021-12-24 10:47:13 +0700

    lab9_14

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const errorController = require('./controllers/error');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf =  require('csurf');
+const csrf = require('csurf');
 const flash = require('connect-flash');
 
 const User = require('./models/user');
@@ -36,18 +36,23 @@ app.use(csrfProtecttion);
 app.use(flash());
 
 app.use((req, res, next) => {
-    if(!req.session.user){
+    if (!req.session.user) {
         return next();
     }
     User.findById(req.session.user._id)
         .then(user => {
+            if (!user) {
+                return next();
+            }
             req.user = user;
             next();
         })
-        .catch(err => { console.log(err) })
+        .catch(err => {
+            throw new Error(err);
+        })
 })
 
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
     res.locals.isAuthenticated = req.session.isLoggedIn;
     res.locals.csrfToken = req.csrfToken();
     next()
