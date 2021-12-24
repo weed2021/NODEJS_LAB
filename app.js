417788d6632ed7cfcda97e9f54b7ad05071a0dc3
bokute2010 <lequanvt299@gmail.com>
2021-12-24 12:25:08 +0700

    lab9_19

--- app.js
+++ app.js
@@ -36,11 +36,20 @@ app.use(csrfProtecttion);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next()
+})
+
+app.use((req, res, next) => {
+    //throw new Error('SyncDummy')
     if (!req.session.user) {
         return next();
     }
+    
     User.findById(req.session.user._id)
         .then(user => {
+            //throw new Error('SyncDummy')
             if (!user) {
                 return next();
             }
@@ -48,15 +57,12 @@ app.use((req, res, next) => {
             next();
         })
         .catch(err => {
-            throw new Error(err);
+            next(new Error(err));
+            // throw new Error(err);
         })
 })
 
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next()
-})
+
 
 
 app.use('/admin', adminRoutes);
@@ -68,7 +74,13 @@ app.get('/500',errorController.get500)
 app.use(errorController.get404)
 
 app.use((error,req,res,next)=>{
-    res.redirect('/500')
+    //res.redirect('/500')
+    res.status(500).render('500',
+        {
+            pageTitle: 'Error!',
+            path: '/500',
+            isAuthenticated: req.session.isLoggedIn
+        })
 })
 
 mongoose
