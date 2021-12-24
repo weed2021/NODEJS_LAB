ef9d172c89a441cf73f3106c16801da7eb60181a
bokute2010 <lequanvt299@gmail.com>
2021-12-24 15:17:59 +0700

    lab10_5

--- app.js
+++ app.js
@@ -22,13 +22,25 @@ const store = new MongoDBStore({
 const csrfProtecttion = csrf();
 
 const fileStorage = multer.diskStorage({
-    destination: (req,file,cb) =>{
-        cb(null,'images')
+    destination: (req, file, cb) => {
+        cb(null, 'images')
     },
-    filename:(req,file,cb)=>{
-        cb(null,new Date().toISOString() + '-' +file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname)
     }
 })
+const fileFilter = (req, file, cb) => {
+    if (
+        file.filename === 'image/png'
+        || file.filename === 'image/jpg'
+        || file.filename === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views')
@@ -38,7 +50,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({storage: fileStorage}).single('image'));
+app.use(multer({ storage: fileStorage,fileFilter: fileFilter }).single('image'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }))
@@ -58,7 +70,7 @@ app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
     }
-    
+
     User.findById(req.session.user._id)
         .then(user => {
             //throw new Error('SyncDummy')
@@ -81,11 +93,11 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500',errorController.get500)
+app.get('/500', errorController.get500)
 
 app.use(errorController.get404)
 
-app.use((error,req,res,next)=>{
+app.use((error, req, res, next) => {
     //res.redirect('/500')
     res.status(500).render('500',
         {
