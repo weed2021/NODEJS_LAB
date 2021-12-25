cccbf5d36669f77fc8c2fa694cb7246554cb32fd
bokute2010 <lequanvt299@gmail.com>
2021-12-25 12:40:29 +0700

    lab10_14

--- controllers/shop.js
+++ controllers/shop.js
@@ -187,10 +187,26 @@ exports.getInvoice = (req, res, next) => {
             // res.setHeader('Content-Type', 'application/pdf');
             // res.setHeader('Content-Disposition','inline; filename="' + invoiceName + '"');
             pdfDoc.pipe(fs.createWriteStream(invoicePath));
-            
             pdfDoc.pipe(res);
-            pdfDoc.text('I love you!')
+            pdfDoc.fontSize(24).text('Invoice',{
+                underline: true
+            });
+            pdfDoc.text('---------------------')
+            let totalPrice = 0;
+
+            order.products.forEach(prod => {
+                totalPrice += prod.product.price * prod.quantity;
+                pdfDoc.fontSize(14).text(
+                    prod.product.title + '-' +  
+                    prod.quantity + ' x $' +
+                    prod.product.price                   
+                );
+            });
+            pdfDoc.text('---------------------')
+            pdfDoc.text('Total price: $' + totalPrice)
+            
             pdfDoc.end(); //To end write file
+
             // fs.readFile(invoicePath, (err, data) => {
             //     if (err) {
             //         return next(err);
