e80f634f70018fc46fbcffd5b74271a6a13d25c1
bokute2010 <lequanvt299@gmail.com>
2021-12-26 11:10:24 +0700

    lab10_22

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,13 +8,28 @@ const PDFDocument = require('pdfkit');
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+
+    let totalItems;
     Product.find()
+        .countDocuments() //Count number of doccuments found
+        .then(numProducts => {
+            totalItems = numProducts
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
-            res.render('shop/product-list', {
+            res.render('shop/index', {
                 prods: products,
-                pageTitle: 'All Products',
-                path: '/products',
-                isAuthenticated: req.session.isLoggedIn
+                pageTitle: 'Shop',
+                path: '/',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
